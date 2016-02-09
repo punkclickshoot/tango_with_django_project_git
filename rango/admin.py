ccecab3edfaacd5ffba33d9f8a837c42c7fdc226
martinlafreniere <punkclickshoot@gmail.com>
2016-02-08 21:04:17 -0600

    Imac

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page, PageAdmin
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
--- rango/models.py
+++ rango/models.py
@@ -2,11 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib import admin
+from django.template.defaultfilters import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -22,4 +30,5 @@ class PageAdmin(admin.ModelAdmin):
 
     def __str__(self):      #For Python 2, use __str__ on Python 3
         return self.title
+
 
