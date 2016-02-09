25e184823b6dc3994279b1f3b11feeed71911a68
Marty <punkclickshoot@gmail.com>
2016-02-09 12:14:10 -0600

    Testing

--- populate_rango.py
+++ populate_rango.py
@@ -14,21 +14,21 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/", views=12)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/", views=34)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/", views=65)
 
     django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/", views=13)
 
     add_page(cat=django_cat,
         title="Django Rocks",
